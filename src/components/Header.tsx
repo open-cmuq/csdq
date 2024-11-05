6ab75beacbcd44243673118919317ced7a2c1c4f
Mohammed Al-Jawaheri <mjawaheri02@gmail.com>
2024-11-05 11:04:20 +0300

    changed CS letterhead

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,12 @@ export default function Header() {
     <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         {/* Logo Section */}
-        <div className="flex items-center">
+        <div className="flex-shrink-0">
           <img
-            className="w-10 h-10 object-contain hover:rotate-12 transition-transform duration-300 ease-in-out cursor-pointer"
-            src="/images/cs_logo.png"
+            className="w-60 h-20 object-contain hover:rotate-12 transition-transform duration-300 ease-in-out cursor-pointer flex-shrink-0"
+            src="/images/cs-header.png"
             alt="SCS Qatar Logo"
           />
-          <span className="ml-3 text-base sm:text-lg font-bold text-black hidden sm:inline">
-            Computer Science Qatar
-          </span>
         </div>
 
         {/* Navigation */}
